76d8457e1d6868d9cf6c9aa6b47f4bc86c0d3e22
jayant413 <jayantsawarkar4103@gmail.com>
2023-06-13 23:24:20 +0530

    updated

---
+++ components/About.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import AboutImg from "../public/assets/about.jpg";
+
+const About = () => {
+  return (
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+            About
+          </p>
+          <h2 className="py-4">Who I Am</h2>
+          <p className="p-2 text-gray-300 text-justify ">
+            Hello , Jayant here. I am an updergradute student in Computer
+            Science from ZCOER , SPPU pune , India. A Full-Stack web developer
+            in search of opportunity to contribute my skills. I am passionate
+            about learning new technologies to adapt as per work. As a web
+            developer more interested in learning &#38; building responsive ,
+            interactive possibly best websites as per my experience using
+            front-end and back-end technologies to make a remarkable experience
+            for visitors.
+          </p>
+
+          <p className="p-2 text-gray-300 text-justify">
+            Also you can check out my project below
+            <br /> Thanks.
+          </p>
+
+          <br />
+          <Link href="/#projects">
+            <i
+              className="p-2 text-slate-400 cursor-pointer"
+              style={{ decoration: "none" }}
+            >
+              &rarr; Check out some of my latest projects.
+            </i>
+          </Link>
+        </div>
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-500  rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <Image src={AboutImg} className="rounded-xl" alt="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
+
+{
+  /* <p className="p-2 text-gray-300 text-justify">
+Hey there, Jayant in the house! I'm an enthusiastic undergraduate
+student majoring in Computer Science at ZCOER, SPPU, Pune, India. As
+a full-stack web developer, I'm on the hunt for exciting
+opportunities to showcase my skills and make a real impact. I have
+an insatiable passion for learning and staying up-to-date with the
+latest technologies, always ready to adapt and tackle any web
+development challenge that comes my way.
+</p>
+<p className="p-2 text-gray-300 text-justify">
+What really gets my heart racing is the chance to create
+jaw-dropping, responsive, and interactive websites. I'm constantly
+pushing the boundaries of what's possible, leveraging my front-end
+and back-end expertise to craft the absolute best digital
+experiences for visitors. Trust me, there's nothing more thrilling
+than wowing users with seamless designs and immersive interactions.
+</p>
+<p className="p-2 text-gray-300 text-justify">
+So, buckle up and join me on this exhilarating journey. Together,
+we'll build exceptional websites that leave a lasting impression.
+Get ready to unleash the full potential of technology and make a
+significant impact in the world of web development!
+</p> */
+}
---
+++ pages/movix.jsx
@@ -0,0 +1,78 @@
+import Image from 'next/image';
+import React from 'react';
+import movixImg from '../public/assets/projects/movix.png';
+import { RiRadioButtonFill } from 'react-icons/ri';
+import Link from 'next/link';
+
+const twitch = () => {
+  return (
+    <div className='w-full'>
+      <div className='w-screen h-[50vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+        <Image
+          className='absolute z-1'
+          layout='fill'
+          objectFit='cover'
+          src={movixImg}
+          alt='/'
+        />
+        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+          <h2 className='py-2'>Movix</h2>
+          <h3>Vite / SASS / Redux</h3>
+        </div>
+      </div>
+
+      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
+        <div className='col-span-4'>
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            This appplication was built using NEXT.JS and is styled with
+            Tailwind CSS. The application is hosted statically using Vercel.
+            This is a mobile responsive recreation of Twitch.tv and features
+            Next/Auth.js for authentication. Users may choose to be
+            authenticated with either a Github account or a Google account. A
+            few features to note with this project are lazy loading images using
+            the Image component, built in routing, and Next/Auth Context.
+          </p>
+          <a
+            href='https://github.com/fireclint/twitch-nextjs-tailwind'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+          </a>
+          <a
+            href='https://movix-by-js.netlify.app'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4 '>Demo</button>
+          </a>
+
+        </div>
+        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-500 hover:shadow-gray-300  rounded-xl py-4'>
+          <div className='p-2'>
+            <p className='text-center font-bold pb-2'>Technologies</p>
+            <div className='grid grid-cols-3 md:grid-cols-1'>
+              <p className='text-gray-300 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> React JS - Vite
+              </p>
+              <p className='text-gray-300 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> SASS
+              </p>
+              <p className='text-gray-300 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Redux ToolKit
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href='/#projects'>
+        <button className="px-8 py-2 mt-4">Back</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default twitch;
