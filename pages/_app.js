e0c368c651aba055f100b03da2cdca0acb68338a
Jayant Sawarkar <111290376+jayant413@users.noreply.github.com>
2023-06-29 09:10:59 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import Navbar from '../components/Navbar';
 import '../styles/globals.css';
-import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
