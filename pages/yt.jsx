84849bd55585250930c49b0823b00e79c056854e
jayant413 <jayantsawarkar4103@gmail.com>
2024-03-12 18:32:45 +0530

    Resume updated

--- pages/yt.jsx
+++ pages/yt.jsx
@@ -43,7 +43,7 @@ const crypto = () => {
             <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </a>
           <a
-            href="https://youtube-by-js.vercel.app/ "
+            href="https://youtube-by-js.vercel.app "
             target="_blank"
             rel="noreferrer"
           >
